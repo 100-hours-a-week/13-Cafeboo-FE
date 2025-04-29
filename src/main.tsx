d84044d1697f7b38752297632d3d97af3cf7d41c
leenaheun <skdms1015@konkuk.ac.kr>
2025-04-29 17:46:16 +0900

    chore: tanstack-query 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import App from './App';
+import './styles/global.scss';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,     
+      retry: 1,                       
+      refetchOnWindowFocus: false     
+    }
+  }
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+  </StrictMode>
+);
