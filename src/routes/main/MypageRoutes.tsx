711b6986fc9fe826dbaab6fd884473ec0b59aa9b
leenaheun <skdms1015@konkuk.ac.kr>
2025-04-30 16:26:57 +0900

    🐛 fix: 모든 route에 notfoundpage 처리

--- src/routes/main/MypageRoutes.tsx
+++ src/routes/main/MypageRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import MypagePage from '@/pages/main/mypage/MypagePage';
 import MypageEditPage from '@/pages/main/mypage/MypageEditPage';
+import NotFoundPage from '@/pages/common/NotFoundPage';
 
 const MypageRoutes = () => (
   <Routes>
     <Route path="/" element={<MypagePage />} />
     <Route path="edit" element={<MypageEditPage />} />
+    <Route path="*" element={<NotFoundPage />} /> 
   </Routes>
 );
 
--- src/routes/main/ReportRoutes.tsx
+++ src/routes/main/ReportRoutes.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 import ReportPage from '@/pages/main/report/ReportPage';
+import NotFoundPage from '@/pages/common/NotFoundPage';
 
 const ReportRoutes = () => (
   <Routes>
     <Route path="/" element={<ReportPage />} />
+    <Route path="*" element={<NotFoundPage />} /> 
   </Routes>
 );
 
