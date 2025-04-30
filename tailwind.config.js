736e50ef2ea649ef13040cd31190143ef3757c8e
leenaheun <skdms1015@konkuk.ac.kr>
2025-05-01 02:03:20 +0900

    ✨ feat: 반응형 너비 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          sans: ['Pretendard', '-apple-system', 'BlinkMacSystemFont', 'system-ui', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif'],
-        },
-      },
-    },
-    plugins: [],
-  }
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
