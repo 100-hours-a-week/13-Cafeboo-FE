17e7b9167d62a8a454cdd0edd8073eed1c463fea
leenaheun <skdms1015@konkuk.ac.kr>
2025-07-16 16:54:02 +0900

    ✨ feat: 이벤트 히어로 배너 추가

--- src/routes/main/MainRoutes.tsx
+++ src/routes/main/MainRoutes.tsx
@@ -3,10 +3,12 @@ import DiaryRoutes from './DiaryRoutes';
 import ReportRoutes from './ReportRoutes';
 import MypageRoutes from './MypageRoutes';
 import CoffeeChatRoutes from './CoffeeChatRoutes';
+import EventPage from '@/pages/common/EventPage';
 import NotFoundPage from '@/pages/common/NotFoundPage';
 
 const MainRoutes = () => (
   <Routes>
+    <Route path='event/*' element={<EventPage />} />
     <Route path="diary/*" element={<DiaryRoutes />} />
     <Route path="report/*" element={<ReportRoutes />} />
     <Route path="mypage/*" element={<MypageRoutes />} />
