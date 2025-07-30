f03efc0d0b8414dcc414cb75d782366394467209
leenaheun <skdms1015@konkuk.ac.kr>
2025-07-30 09:54:00 +0900

    ⚡️ perf: js 빌드 최적화 3단계

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,32 @@ export default defineConfig({
       brotliSize: true,
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          kakaoMap: ['react-kakao-maps-sdk'],
+          reactQuery: ['@tanstack/react-query'],
+          zustand: ['zustand'],
+          form: ['react-hook-form', '@hookform/resolvers', 'zod'],
+          radix: [
+            '@radix-ui/react-accordion',
+            '@radix-ui/react-dialog',
+            '@radix-ui/react-dropdown-menu',
+            '@radix-ui/react-label',
+            '@radix-ui/react-popover',
+            '@radix-ui/react-select',
+            '@radix-ui/react-slider',
+            '@radix-ui/react-slot',
+            '@radix-ui/react-toggle',
+            '@radix-ui/react-toggle-group',
+          ],
+          dateFns: ['date-fns'],
+          socket: ['@stomp/stompjs', 'sockjs-client'],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
